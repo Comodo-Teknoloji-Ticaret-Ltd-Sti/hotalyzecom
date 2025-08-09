ed8296ca4d4b78ec844859399c7a2f6d96810210
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2025-08-10 00:25:26 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
+  basePath: isProd ? '/hotalyzecom' : '',
+  assetPrefix: isProd ? '/hotalyzecom/' : '',
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
