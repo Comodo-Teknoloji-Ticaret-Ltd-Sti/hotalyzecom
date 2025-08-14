6546b0c885bf41c547d4c2aed41b498c67f78067
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2025-08-14 22:19:58 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  basePath: isGitHubPages ? '/hotalyzecom' : '',
-  assetPrefix: isGitHubPages ? '/hotalyzecom/' : '',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
